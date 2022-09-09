50b98ff34577abb3708a62611822ca7b6f537c6a
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-09 14:58:10 +0530

    Welcome to line comparision computation program

---
+++ Line_Comparision.java
@@ -0,0 +1,10 @@
+package day8_LineCompare;
+
+public class Line_Comparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+
+}
