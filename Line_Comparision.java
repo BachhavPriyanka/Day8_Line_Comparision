cad82e316a667b858f817ccbcb557d8ac8b38752
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-09 16:54:40 +0530

    Created different Methods

--- Line_Comparision.java
+++ Line_Comparision.java
@@ -1,48 +1,69 @@
 package day8_LineCompare;
+import java.util.*;
 
+class Coordinates {
+	Scanner sc = new Scanner(System.in);
+		// instance variables
+	static double x1, x2, y1, y2, x3, x4, y3, y4;
+	static int distance1, distance2;
+
+	// Computation for point1 & point2
+	public void point1() {
+		System.out.println("enter x1 & y1 values of first point");
+		x1 = sc.nextDouble();
+		y1 = sc.nextDouble();
+	}
+
+	public void point2() {
+		System.out.println("enter x2 & y2 values of second point");
+		x2 = sc.nextDouble();
+		y2 = sc.nextDouble();
+	}
+
+	public void distanceBetweenPoint1Point2() {
+		distance1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Distance between two points = " + distance1);
+	}
+
+	// Computation for point3 & point4
+	public void point3() {
+		System.out.println("enter x3 & y3 values of third point");
+		x3 = sc.nextDouble();
+		y3 = sc.nextDouble();
+	}
+
+	public void point4() {
+		System.out.println("enter x2 & y2 values of second point");
+		x4 = sc.nextDouble();
+		y4 = sc.nextDouble();
+	}
+
+	public void distanceBetweenPoint3Point4() {
+		distance2 = (int) Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+		System.out.println("Distance between two points = " + distance2);
+	}
+
+	// checking for line equality
+	public void CompareLinesUsingEqualToMethod() {
+		String str1 = Double.toString(distance1);
+		String str2 = Double.toString(distance2);
+
+		if (str1.equals(str2) == true)
+			System.out.println("Both lines are equal");
+			else
+				System.out.println("Not both lines are equal");
+		}
+	}
 public class Line_Comparision {
-	public static Double length(int x1, int y1, int x2, int y2) {
-		  Double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-	        System.out.println("x1 : "+x1);
-	        System.out.println("y1 : "+y1);
-	        System.out.println("x2 : "+x2);
-	        System.out.println("y2 : "+y2);
-	        return length;
-}
-	public static void main(String[] args) {
-		int l1x1, l1y1, l1x2, l1y2;
-        int l2x1, l2y1, l2x2, l2y2;
-        Double length1, length2;
-        boolean isEqual;
-        int compareValue;
-
-        l1x1 = 2;
-        l1y1 = 3;
-        l1x2 = 4;
-        l1y2 = 10;
-        length1 = length(l1x1, l1y1, l1x2, l1y2);
-        System.out.println("Length 1 : "+length1);
-
-        l2x1 = 2;
-        l2y1 = 3;
-        l2x2 = 4;
-        l2y2 = 8;
-        length2 = length(l2x1, l2y1, l2x2, l2y2);
-        System.out.println("Length 2 : "+length2);
-
-        isEqual = length1.equals(length2);
-        if(isEqual)
-            System.out.println("The Lines are equal");
-        else
-            System.out.println("The lines are not equal");
-
-        compareValue = length1.compareTo(length2);
-        if(compareValue < 0)
-            System.out.println("Line 1 is smaller than Line 2");
-        else if(compareValue == 0)
-            System.out.println("Line 1 is equal to Line 2");
-        else
-            System.out.println("Line 1 is greater than Line 2");
-
-    }
+public static void main(String[] args) {
+		Coordinates coordinate = new Coordinates();
+		// Base class method calling
+		coordinate.point1();
+		coordinate.point2();
+		coordinate.distanceBetweenPoint1Point2();
+		coordinate.point3();
+		coordinate.point4();
+		coordinate.distanceBetweenPoint3Point4();
+		coordinate.CompareLinesUsingEqualToMethod();
+	}
 }
