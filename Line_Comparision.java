9a2b825002e3f8bb58b74e64b8f2d54156a5fad1
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-09 15:09:30 +0530

    Calculating lengths

--- Line_Comparision.java
+++ Line_Comparision.java
@@ -1,10 +1,22 @@
 package day8_LineCompare;
-
+import java.util.Random;
 public class Line_Comparision {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-	}
+		System.out.println("Welcome to Line Comparision Computation");
 
+        Random rand = new Random();
+        int x1 = rand.nextInt(10);
+        int x2 = rand.nextInt(10);
+        int y1 = rand.nextInt(10);
+        int y2 = rand.nextInt(10);
+
+        Double length = Math.sqrt((x2-x1) ^ 2 + (y2-y1) ^ 2);
+        System.out.println("x1 : "+x1);
+        System.out.println("x2 : "+x2);
+        System.out.println("y1 : "+y1);
+        System.out.println("y2 : "+y2);
+        System.out.println("Length of the line is : "+length);
+    }
 }
